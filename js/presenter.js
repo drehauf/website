43ddd4717bd3da8473b9180111ab5b794ba3f0a0
Anton <qau@gmx.net>
2019-04-06 14:20:05 +0200

    created template and started carousel implementation with node cloning

--- js/presenter.js
+++ js/presenter.js
@@ -3,47 +3,67 @@ let presenter = {
   getElement: (id) => (document.getElementById(id)),
   getElements: (className) => (document.getElementsByClassName(className)),
 
-  carousel: {
-    images: [
-      {
-        src: 'img/carousel/frame1.jpg',
-        alt: 'conference hall'
-      },
-      {
-        src: 'img/carousel/frame2.jpg',
-        alt: 'sound equipment'
-      },
-      {
-        src: 'img/carousel/frame3.jpg',
-        alt: 'mixer knobs'
-      },
-      {
-        src: 'img/carousel/frame4.jpg',
-        alt: 'concert at night'
-      },
-      {
-        src: 'img/carousel/frame5.jpg',
-        alt: 'illuminated staircase'
-      },
-      {
-        src: 'img/carousel/frame6.jpg',
-        alt: 'rehearsal room'
-      }
-    ]
+  carousel: [
+    {
+      src: 'img/carousel/frame1.jpg',
+      alt: 'conference hall',
+      caption: "Wir können mehr!",
+      text: "Ob Konzert, Party, Kongress, Hochzeit, Geburtstag: Ihr Partner in Sachen Veranstaltung!",
+      href: "#services",
+      actiontext: "LEISTUNGEN"
+    },
+    {
+      src: 'img/carousel/frame2.jpg',
+      alt: 'sound equipment'
+    },
+    {
+      src: 'img/carousel/frame3.jpg',
+      alt: 'mixer knobs'
+    },
+    {
+      src: 'img/carousel/frame4.jpg',
+      alt: 'concert at night'
+    },
+    {
+      src: 'img/carousel/frame5.jpg',
+      alt: 'illuminated staircase'
+    },
+    {
+      src: 'img/carousel/frame6.jpg',
+      alt: 'rehearsal room'
+    }
+  ],
+
+  setTemplateData: function(element, data) {
+    for (let key in data) {
+      element.innerHTML = element.innerHTML.replace('%' + key, data[key]);
+      console.log(element.innerHTML);
+    }
   },
 
-  start: function() {
-    let images = presenter.carousel.images;
-    let div = presenter.getElement('slider')
+  cloneTemplate: function(templateId, divId, callback) {
+    let template = presenter.getElement(templateId);
+    let div = presenter.getElement(divId);
+    // div.parent.firstChild.remove();
+    let clone = template.cloneNode(true);
+    callback(clone);
+    div.append(clone);
+  },
+
+  showCarousel: function(timePerImageMs) {
+    let data = presenter.carousel;
     let i = 0;
     setInterval(() => {
-      console.log(div.innerHTML);
-      div.innerHTML = div.innerHTML.replace('%src', images[i].src);
-      div.innerHTML = div.innerHTML.replace('%alt', images[i].alt);
+      presenter.cloneTemplate('carousel', 'slider', (clone) => {
+        let image = presenter.getElement('image-placeholder');
+        image.setAttribute('src', '%src');
+        image.setAttribute('alt', '%alt');
+        presenter.setTemplateData(clone, data[i])
+      });
       i++;
-      i = i > images.length - 1 ? 0 : i;
-    }, 5000);
+      i = i > data.length - 1 ? 0 : i;
+    }, timePerImageMs * 1000);
   }
 };
 
-presenter.start(presenter.start);
+presenter.showCarousel(3)
