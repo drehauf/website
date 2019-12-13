97782f844062eed47d9bb26d30d7ff8ff561de56
TerminatorMP <kingmarcel98@gmx.de>
2019-12-13 19:12:37 +0100

    scss implemented

--- src/components/web/Sections.js
+++ src/components/web/Sections.js
@@ -3,7 +3,7 @@ import About from './sections/About';
 import Services from './sections/Services';
 import Credentials from './sections/Credentials';
 import Contact from './sections/Contact';
-import '../../stylesheets/Sections.css';
+import '../../stylesheets/Sections.scss';
 
 class Sections extends Component {
 
