f8628ff17cfacbd7210442db3b6d4b5b1f918777
Eddie <eddie.muss@archipinion.com>
2019-12-15 02:29:59 +0100

    Add smooth scrolling

--- src/components/essentials/Button.js
+++ src/components/essentials/Button.js
@@ -3,7 +3,7 @@ import { HashLink } from "react-router-hash-link";
 import '../../stylesheets/Button.scss';
 
 const Button = ({ href, text, external }) => {
-  let link = <HashLink to={href}>{text}</HashLink>;
+  let link = <HashLink smooth to={href}>{text}</HashLink>;
   
   if (external) {
     link = <a href={href}>{text}</a>;
