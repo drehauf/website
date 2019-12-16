1ec053e2e6e5e666aadcccd4733cc7e72c939c3e
Eddie <eddie.muss@archipinion.com>
2019-12-16 13:01:14 +0100

    fixed Table but have to clean up

--- src/components/web/inventory/Inventory.js
+++ src/components/web/inventory/Inventory.js
@@ -20,12 +20,16 @@ const Inventory = (props) => {
   const [ didLoad, setDidLoad ] = useState(false);
 
   useEffect(() => {
-    data.forEach((file) => {
-      CSVParser.get(file, (data) => {
-        setPages((prev) => [...prev, data]);
+    fetchTabelData();
+  }, []);
+
+  const fetchTabelData = () => {
+    data.forEach(file => {
+      CSVParser.get(file, data => {
+        setPages(prev => [...prev, data]);
       });
     });
-  }, []);
+  }
 
   useEffect(() => {
     if (pages.length === data.length) {
@@ -58,36 +62,43 @@ const Inventory = (props) => {
     console.log('next');
   };
 
-  const table = () => (
-    <div className='table_wrapper'>
-      <table className='table'>
-        <thead className="table_head">
-          <tr>
-            <th colSpan='2'>Bezeichnung</th>
-            <th>Anzahl</th>
-          </tr>
-        </thead>
-        <tbody>
-          {createBody(pages[0].data)}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colSpan='3'>
-              <Pagination
-                previous={pages[1].title}
-                current={pages[0].title}
-                next={pages[4].title}
-                handlePreviousPage={handlePreviousPage}
-                handleNextPage={handleNextPage}
-              />
-            </td>
-          </tr>
-        </tfoot>
-      </table>
-    </div>
-  );
+  const table = () => {
+    console.log('table');
+    
+    return (
+      <div className='table_wrapper'>
+        <table className='table'>
+          <thead className="table_head">
+            <tr>
+              <th colSpan='2'>Bezeichnung</th>
+              <th>Anzahl</th>
+            </tr>
+          </thead>
+          <tbody>
+            {createBody(pages[0].data)}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan='3'>
+                <Pagination
+                  previous={pages[1].title}
+                  current={pages[0].title}
+                  next={pages[4].title}
+                  handlePreviousPage={handlePreviousPage}
+                  handleNextPage={handleNextPage}
+                />
+              </td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
+  
 
-  return didLoad ? table : null;
+  const renderTable = didLoad ? table() : 'Hello there';
+
+  return renderTable;
 
 }
 
