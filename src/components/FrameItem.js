8ce9bcc93ca7f50cad17755855eda21d19b391c5
Anton <qau@gmx.net>
2019-12-20 16:44:08 +0100

    Fix carousel frame buttons

--- src/components/FrameItem.js
+++ src/components/FrameItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import FrameOverlay from 'components/FrameOverlay';
+import Button from 'components/essentials/Button';
 import '../stylesheets/Carousel.scss';
 
 const FrameItem = (props) => {
+  const button = <Button href={props.href} text={props.actiontext} external={props.external}/>
   return (
     <div>
       <img src={props.src} alt={props.alt} className="carousel_image" />
       <FrameOverlay
         caption={props.caption}
         text={props.text}
-        href={props.href}
-        actiontext={props.actiontext}
-        external={props.external}
+        button={button}
       />
     </div>
   );
