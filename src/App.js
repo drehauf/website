d6732604a07b69723966672f50fb8ed384896920
Anton Quietzsch <qau@gmx.net>
2020-05-01 12:44:28 +0200

    [ADD] ScrollToTop to fix page scroll when changing route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import ScrollToTop from 'ScrollToTop';
 import Main from './components/Main';
 import Legal from './components/footer/Legal';
 import Privacy from "./components/footer/Privacy";
@@ -8,8 +9,9 @@ import './stylesheets/index.scss';
 const App = () => {
 
   return (
-    <Fragment>
-      <Router>
+    <Router>
+      <Fragment>
+        <ScrollToTop/>
         <Switch>
           <Route path="/impressum">
             <Legal/>
@@ -24,8 +26,8 @@ const App = () => {
             <Main/>
           </Route>
         </Switch>
-      </Router>
-    </Fragment>
+      </Fragment>
+    </Router>
   );
 }
 
